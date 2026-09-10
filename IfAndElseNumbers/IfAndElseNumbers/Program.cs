a8176f20e1dbbb250665aa7c5db6eae6c7796a75
Opilane Techno+TLN <nominalhydroxides2@gmail.com>
2026-09-10 10:51:35 +0300

    Uus projekt: If ja Else tingimuslaused numbritega

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Channels;
+
+namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number:");
+
+            // Konsoolis sisestame numbri.
+            // See number salvestatakse string andmetüüpi.
+            // Toimub kontroll, kas sisestatud väärtus on täis- või ujukomaarv.
+            // Kasutada if ja else tingimuslauseid.
+
+            // Muutuja nimega number, kuhu salvestatakse kasutaja sisestatud tekst.
+            string number = Console.ReadLine();
+
+            // Kontrollime, kas sisestatud väärtus on täis- või ujukomaarv.
+            if (double.TryParse(number, out double result))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Sisestasid numbri. Väga hea.");
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Midagi läks untsu :( Proovi uuesti.");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+            }
+        }
+    }
+}
