90d9304a30d51640a660a15346ca89882e4d929f
Opilane Techno+TLN <nominalhydroxides2@gmail.com>
2026-09-10 09:47:05 +0300

    Uus projekt: If ja Else tingimuslaused.
    Väga sigma värk btw

---
+++ IfAndElse/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        // Projekt nimetusega IfAndElse, mille sees asub class nimega Program.
+        // See klass sisaldab Main meetodit, mis on programmi sisenemispunkt. 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi:");
+
+            // Muutuja nimega name, kuhu salvestatakse kasutaja sisestatud tekst.
+            string name = Console.ReadLine();
+
+            // ! tähendab "ei ole" ja == tähendab "on võrdne".
+            if (name != "")
+            {
+                // Kui kasutaja sisestab midagi, siis muutub taustavärv roheliseks.
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Sisestasid enda nime.");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                // Kui sisend (Sisestatud nimi) on vale või tühi, siis muutub taustavärv punaseks.
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Midagi läks untsu :( Proovi uuesti.");
+                // Lisaks kostub 2 piiksu.
+                Console.Beep();
+                // Ja oodatakse 1 sekund enne programmi lõppu.
+                Thread.Sleep(1000);
+                Console.Beep();
+            }
+        }
+    }
+}
