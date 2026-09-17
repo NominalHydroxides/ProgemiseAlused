dc0f214d8abc6f281c8aac0f759212d73ac061d2
Opilane Techno+TLN <nominalhydroxides2@gmail.com>
2026-09-17 10:46:33 +0300

    Muudatus: Lisatud kommentaarid.

--- IfAndElseNesting3/IfAndElseNesting3/Program.cs
+++ IfAndElseNesting3/IfAndElseNesting3/Program.cs
@@ -17,21 +17,25 @@
 
             if (y == 12)
             {
+                // Kui Y väärtus on 12, siis konsool väljastab teksti: Y võrdub 12.
                 Console.WriteLine("Y võrdub 12");
             }
             else if (y > 20)
             {
                 if (y == 23)
                 {
+                    // Kui Y väärtus on suurem kui 20 ja on 23, siis konsool väljastab teksti: Y võrdub 23.
                     Console.WriteLine("Y võrdub 23");
                 }
                 else
                 {
+                    // Kui Y väärtus on suurem kui 20 ja on vahemikus 21 kuni 22 ning 24 kuni lõpmatus, siis konsool väljastav teksti: Y on vahemikus 21 kuni 22 ja 24 kuni lõpmatus.
                     Console.WriteLine("Y on vahemikus 21 kuni 22 ja 24 kuni lõpmatus");
                 }
             }
             else
             {
+                // Kui Y väärtus ei kattu ülejäänud väärtustega, siis konsool väljastab teksti: Mingid kahtlased väärtused.
                 Console.WriteLine("Mingid kahtlased väärtused");
             }
         }
